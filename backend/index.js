bcb49045b119fd091ee7435183a0df25bbc10d07
IT18121834_Jayasekara A.S <it18121834@my.sliit.lk>
2021-10-01 00:44:11 +0530

    code refactored

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,35 @@
 require('dotenv/config')
-
 const express = require('express');
-const { LocalStorage } = require("node-localstorage");
 const app = express();
 const cors = require("cors");
 require('dotenv').config()
 const passport = require('passport');
 const session = require('express-session');
-const fbAuth = require('./routes/fbAuth')
-app.use(express.json());
-app.use(cors());
-
-const facebookStrategy = require('passport-facebook').Strategy
-
-
-// Google OAuth Routes
-
 const bodyParser = require('body-parser');
+
+const googleOAuthRoute = require("./routes/googleAuth/googleAuthRoute");
+const fbAuth = require('./routes/fbAuth')
+const ZoomOAuthRoute = require("./routes/zoom.routes/Zoom.oauth.route");
+
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(require('cookie-parser')());
 app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cors());
+
+
 // Google OAuth Routes
-const googleOAuthRoute = require("./routes/googleAuth/googleAuthRoute");
 app.use("/googleOAuth",googleOAuthRoute);
 
 // Zoom OAuth Routes
-const ZoomOAuthRoute = require("./routes/zoom.routes/Zoom.oauth.route");
 app.use("/zoomOAuth", ZoomOAuthRoute);
 
+// Facebook OAuth Routes
 app.use('/fbOauth', fbAuth);
+
 
 app.listen(process.env.PORT, () => {
     console.log("🚀 Server started on port " + process.env.PORT);
