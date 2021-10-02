84242bafccd0ee50fd0f3959e2d555b330e7f456
IT18121834_Jayasekara A.S <it18121834@my.sliit.lk>
2021-10-03 00:24:19 +0530

    recormeatted the code

--- frontend/src/Components/FacebookComponents/Posts.js
+++ frontend/src/Components/FacebookComponents/Posts.js
@@ -1,25 +1,15 @@
 import React, {useState} from 'react';
-import Button from 'react-bootstrap/Button'
 import {Card} from '@mui/material';
 import Col from 'react-bootstrap/Col';
-import { CardContent, CardHeader } from '@mui/material';
+import {CardContent, CardHeader} from '@mui/material';
 import Typography from '@mui/material/Typography';
 import posts from "./images/posts.png";
 import cancel from "./images/cancel.png";
 import Moment from 'moment';
-
 import axios from "axios";
-import  { Redirect } from 'react-router-dom'
 import swal from "sweetalert";
 
 function Posts({product, name, setDeleteData}) {
-    const getPostData = () => {
-        console.log("eoifjosfi")
-        axios.get("http://localhost:5000/fbOauth/getPagePosts").then(results => {
-            console.log("RESULT>DATA", results.data)
-        })
-    }
-
     const deletePagePost = (postId) => {
         axios.delete('http://localhost:5000/fbOauth/deletePosts', {
             data: {
@@ -28,25 +18,27 @@ function Posts({product, name, setDeleteData}) {
         }).then(results => {
             console.log("REST", results)
         }) && swal({
-            title:`Deleted!`,
-            text:`Successfully deleted your post!`,
-            icon:"success"
+            title: `Deleted!`,
+            text: `Successfully deleted your post!`,
+            icon: "success"
         }) && window.location.reload();
     }
-
-    return(
-        <Col xs={3} key={product.id} >
-            <Card style={{ width: '100rem'}}>
+    return (
+        <Col xs={3} key={product.id}>
+            <Card style={{width: '100rem'}}>
                 <CardContent>
-                    <img src={cancel} height={20} width={20}  style={{marginLeft: "1%", marginBottom:"0%", textAlign:"center"}} onClick={() => deletePagePost(product.id)}/>
-                    <img src={posts} height={80} width={100} style={{marginLeft: "1%", marginBottom:"0%", textAlign:"right"}} /> <br/><br/>
-                    <Typography sx={{ fontSize: 12 }} style={{marginLeft:"2%"}} color="text.secondary" gutterBottom>
-                       Page Name : {name}
+                    <img src={cancel} height={20} width={20}
+                         style={{marginLeft: "1%", marginBottom: "0%", textAlign: "center"}}
+                         onClick={() => deletePagePost(product.id)}/>
+                    <img src={posts} height={80} width={100}
+                         style={{marginLeft: "1%", marginBottom: "0%", textAlign: "right"}}/> <br/><br/>
+                    <Typography sx={{fontSize: 12}} style={{marginLeft: "2%"}} color="text.secondary" gutterBottom>
+                        Page Name : {name}
                     </Typography>
-                    <Typography fontSize={14} style={{marginLeft:"2%"}}>
+                    <Typography fontSize={14} style={{marginLeft: "2%"}}>
                         Message : {product.message}
                     </Typography>
-                    <Typography fontSize={14} style={{marginLeft:"2%"}}>
+                    <Typography fontSize={14} style={{marginLeft: "2%"}}>
                         Created time : {Moment(product.created_time).format('YYYY-MM-DD HH:mm')}
                     </Typography>
                 </CardContent>
